bb2538955f4773aa5800257194609746870b15c1
Lourival Pinheiro <lourivaltpneto@outlook.com>
2024-10-09 12:47:12 -0300

    New bar chart
    Menus have been updated and a bar chart has been inserted.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Setting up the pages' functions
 
-def menu_principal():
+def curriculo():
     # Dictionary 
 
     dicionario = {
@@ -31,7 +31,21 @@ def menu_principal():
             'Junho',
             'Maio',
             'Março'],
-    
+
+    "carga_horaria": [
+       4,
+       15,
+       2,
+       2,
+       2,
+       2,
+       5,
+       40,
+       2,
+       8,
+       10
+    ],
+
     "instituicao": ['Conquer Business School', 
                     'Fundação Bradesco', 
                     'Cefis',
@@ -46,12 +60,12 @@ def menu_principal():
     
     }
 
+
     df = pd.DataFrame(dicionario)
 
     # Page's basic configuration 
 
     st.set_page_config(page_title='Neto Pinheiro', page_icon='neto.png', layout='wide')
-    st.logo('neto.png')
 
     # Links
 
@@ -87,7 +101,88 @@ def menu_principal():
         st.write('''Com um olhar analítico e proativo, estou construindo uma expertise em Contabilidade e Ciência de Dados. Meu objetivo é identificar oportunidades de melhoria e impulsionar o crescimento financeiro, gerando valor tanto para a minha empresa quanto para nossos clientes.''')
     with imagem:
         st.image('team.jpg')
-        
+
+def cursos():
+    # Dictionary
+
+    dicionario = {
+
+    "nome": ['Inteligência Emocional 4.0', 
+            'Microsoft Excel - Básico',
+            'Iniciação Contábil',
+            'Metodologia de escrituração contábil integrada',
+            'Plano de contas contábil',
+            'Rotinas de auxiliar administrativo',
+            'Inteligência Artificial',
+            'Python 3 - Mundo 1',
+            'Python: noções introdutórias',
+            'Certificação Lean Seis Sigma - White Belt',
+            'Do R ao H'],
+    
+    "mes": ['Outubro', 
+            'Maio', 
+            'Agosto',
+            'Agosto',
+            'Agosto',
+            'Agosto',
+            'Junho',
+            'Junho',
+            'Junho',
+            'Maio',
+            'Março'],
+
+    "carga_horaria": [
+       4,
+       15,
+       2,
+       2,
+       2,
+       2,
+       5,
+       40,
+       2,
+       8,
+       10
+    ],
+
+    "instituicao": ['Conquer Business School', 
+                    'Fundação Bradesco', 
+                    'Cefis',
+                    'Cefis',
+                    'Cefis',
+                    'Cefis',
+                    'Conquer Business School',
+                    'Curso em Vídeo',
+                    'Ada Tech',
+                    'FM2S',
+                    'Conquer Business School']
+    
+    }
+
+    df = pd.DataFrame(dicionario)
+    df2 = df[['mes', 'carga_horaria']]
+
+    # Body of the page
+
+    ## Basic configuration
+
+    st.set_page_config(page_title='Projetos', page_icon='neto.png', layout='wide')
+
+    # Body of the page
+
+    st.markdown('# Conhecimento')   
+    st.caption('Fique por dentro de tudo que concerne meus estudos.')
+    
+    st.markdown("# Cursos extra-curriculares")
+    st.caption('Fique por dentro de tudo que concerne meus estudos.')
+    st.table(df)
+    st.markdown('### Quantidade de horas de estudo:')
+    st.bar_chart(df2, x='mes', y='carga_horaria', x_label='Meses', y_label='Carga Horária')
+
+
+
+
+    
 def projetos():
 
     # Basic configuration 
@@ -100,7 +195,8 @@ def projetos():
 
 # Setting up pages
 
-principal = st.Page(menu_principal, title='Neto Pinheiro', default=True)
+principal = st.Page(curriculo, title='Currículo', default=True)
+knowledge = st.Page(cursos, title='Cursos')
 projeto = st.Page(projetos, title='Informação')
 
 # Navigation Bar
@@ -108,6 +204,7 @@ projeto = st.Page(projetos, title='Informação')
 pg = st.navigation(
     {
         "📑 Sobre mim": [principal],
+        "📚 Conhecimento": [knowledge],
         "📂 Projetos": [projeto]
     }
 )
